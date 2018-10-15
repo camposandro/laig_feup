02cb6760d76bdc7f1b575a05e4078ddee3c76a14
sendim <henrique000@live.com.pt>
2018-10-15 16:44:56 +0100

    Update MyTexture.js

--- First project/entities/MyTexture.js
+++ First project/entities/MyTexture.js
@@ -1,20 +1,16 @@
 /**
  * MyTexture class, representing a texture.
  */
-class MyTexture {
+class MyTexture extends CGFtexture{
 
 	/**
      * @constructor
      * @param {id}
      */
     constructor(scene, id, file) {
+        super(scene, file);
         this.id = id;
-        this.loadTex(scene, file);
-    }
-
-    loadTex(scene, file) {
-        this.appearence = new CGFappearance(scene);
-        this.appearence.loadTexture(file);
         //this.appearence.setTextureWrap('REPEAT','REPEAT');
     }
+
 }
