f4d6e3e062acfb0f1c4fe56438ad81a4640a1a21
sendim <henrique000@live.com.pt>
2018-11-14 11:31:29 +0000

    Update MyCircularAnimation.js

--- Second delivery/animations/MyCircularAnimation.js
+++ Second delivery/animations/MyCircularAnimation.js
@@ -15,9 +15,66 @@ class MyCircularAnimation extends MyAnimation {
      */
     constructor(id, span, center, radius, startang, rotang) {
         super(id,span);
-        this.center = center;
+
+        var centerS = center += '';
+        const splitString = centerS.split(" ") ;
+
+        this.centerX = splitString[0];
+        this.centerY = splitString[1];
+        this.centerZ = splitString[2];
+        this.first = true;
+
         this.radius = radius;
         this.startang = startang;
         this.rotang = rotang;
+        this.finished = false;
+        this.totalTime = 0;
+        
+        this.position;
+        this.anteriorPoint = [0,0,0];
+        this.angle = startang;
+
     };
+
+
+    update(currTime) {
+        if(this.finished)
+            return true;
+            
+        var time;
+        if(this.lastCurrTime > 0) {
+            time = currTime - this.lastCurrTime;
+        }
+        else 
+            time = 0;
+
+        this.lastCurrTime = currTime;
+        this.totalTime += time;
+        if((this.totalTime > this.span)){
+            this.finished = true;
+            return true;
+        }
+        
+        this.angle = this.startang + this.rotang / this.span * time;
+        
+        var posX = this.radius * Math.cos(this.angle);
+        var posZ = this.radius * Math.sin(this.angle);
+        
+        this.anteriorPoint = this.position;
+        this.position = [posX, 0, posZ];
+    }
+
+
+    apply(transformationsMatrix) {
+        /*
+        if(this.first){
+            mat4.translate(transformationsMatrix, transformationsMatrix, this.center);
+            this.first = false;
+        }*/
+        if(!this.finished) {
+            mat4.rotate(transformationsMatrix, transformationsMatrix, this.angle, [0,1,0]);
+            return transformationsMatrix;
+        }
+        
+    }
 }
